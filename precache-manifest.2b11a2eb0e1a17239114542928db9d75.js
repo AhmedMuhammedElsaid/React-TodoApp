42d495719753fe2a8a80511fc0f46746c32e24ad
AhmedMuhammedElsaid <ahmed.muhammed.elsaid@gmail.com>
2020-02-01 18:48:02 +0200

    Updates

---
+++ precache-manifest.2b11a2eb0e1a17239114542928db9d75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17299df23b7311eb7ea6fdd740225c47",
+    "url": "/React-TodoApp/./index.html"
+  },
+  {
+    "revision": "784e2bf9779ddc9b566e",
+    "url": "/React-TodoApp/./static/css/main.74741d83.chunk.css"
+  },
+  {
+    "revision": "b5883d4aeb8d804b74eb",
+    "url": "/React-TodoApp/./static/js/2.34f4ab45.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-TodoApp/./static/js/2.34f4ab45.chunk.js.LICENSE"
+  },
+  {
+    "revision": "784e2bf9779ddc9b566e",
+    "url": "/React-TodoApp/./static/js/main.261bf566.chunk.js"
+  },
+  {
+    "revision": "b78bd31538df1ac04951",
+    "url": "/React-TodoApp/./static/js/runtime-main.aca94e75.js"
+  }
+]);
